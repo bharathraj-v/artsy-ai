5aa74cc9019d1aa91a43a6b2b1386ec79878a479
Bharath Raj <bharathrajpalivela@gmail.com>
2023-10-10 22:33:53 +0530

    fix secretkey issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,6 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
