4925f1bff6137af98383e1bc7ea803c7626ecad6
Bharath Raj <bharathrajpalivela@gmail.com>
2023-10-10 22:20:06 +0530

    removed unused secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!0bb4igr=_qg++)t30@60f29f8l5^&6=!4tf!nrb)&qy1f_d4b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
