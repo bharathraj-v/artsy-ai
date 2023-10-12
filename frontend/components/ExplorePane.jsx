a418f2c6a8440c15bcaab535e640bbaaa117795d
Bharath Raj <bharathrajpalivela@gmail.com>
2023-10-12 06:47:44 +0530

    revert hover effects

--- frontend/components/ExplorePane.jsx
+++ frontend/components/ExplorePane.jsx
@@ -1,12 +1,12 @@
 const ExplorePane = ({source}) => {
     return (
              <div className="flex w-128 h-128  ">
-                <img  src = {source} className="w-128 rounded-xl hover:shadow-2xl object-fill h-128"/>
-                <div className="absolute h-128 hover:cursor-pointer opacity-0 hover:opacity-90">
+                <img  src = {source} className="w-128 rounded-xl hover:border-2 hover:shadow-2xl object-fill h-128"/>
+                {/* <div className="absolute h-128 hover:cursor-pointer opacity-0 hover:opacity-90">
                 <div className="flex mt-120  h-10 text-white rounded-b-xl w-128  bg-black items-center justify-center text-xl font-bold">
                     Buy now
                     </div>
-                </div>
+                </div> */}
                 
         </div>
         
