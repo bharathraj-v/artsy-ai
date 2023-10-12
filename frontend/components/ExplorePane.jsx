ad37dd0df50b316cf26b024bdec317c680ddacd7
Bharath Raj <bharathrajpalivela@gmail.com>
2023-10-12 06:39:10 +0530

    update readme and explore pane

--- frontend/components/ExplorePane.jsx
+++ frontend/components/ExplorePane.jsx
@@ -1,7 +1,13 @@
 const ExplorePane = ({source}) => {
     return (
-             <div className="flex w-128 h-128 hover:border-2 ">
+             <div className="flex w-128 h-128  ">
                 <img  src = {source} className="w-128 rounded-xl hover:shadow-2xl object-fill h-128"/>
+                <div className="absolute h-128 hover:cursor-pointer opacity-0 hover:opacity-90">
+                <div className="flex mt-120  h-10 text-white rounded-b-xl w-128  bg-black items-center justify-center text-xl font-bold">
+                    Buy now
+                    </div>
+                </div>
+                
         </div>
         
        
