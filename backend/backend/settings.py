71280cc636b9ca51c9a5bbb61220658c3704f65d
Bharath Raj <bharathrajpalivela@gmail.com>
2023-10-12 05:54:54 +0530

    add host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'artsy-ai-platform.vercel.app' '.vercel.app', 'localhost']
 
 
 # Application definition
